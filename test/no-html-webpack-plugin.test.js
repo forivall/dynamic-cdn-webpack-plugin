5eefb6681569194874d54f71fd5d15132dbe34b9
Emily Marigold Klassen <forivall@gmail.com>
2022-01-19 20:18:44 -0800

    test: fix parallel testing, reenable

--- test/no-html-webpack-plugin.test.js
+++ test/no-html-webpack-plugin.test.js
@@ -8,15 +8,15 @@ const DynamicCdnWebpackPlugin = require('..').default;
 const runWebpack = require('./helpers/run-webpack.js');
 const cleanDir = require('./helpers/clean-dir.js');
 
-t.test('html-webpack-plugin', async t => {
-    await cleanDir(path.resolve(__dirname, './fixtures/output/html-webpack-plugin'));
+t.test('no-html-webpack-plugin', async t => {
+    await cleanDir(path.resolve(__dirname, './fixtures/output/no-html-webpack-plugin'));
 
     await runWebpack({
         context: path.resolve(__dirname, './fixtures/app'),
 
         output: {
             publicPath: '/',
-            path: path.resolve(__dirname, './fixtures/output/html-webpack-plugin'),
+            path: path.resolve(__dirname, './fixtures/output/no-html-webpack-plugin'),
         },
 
         entry: {
@@ -30,12 +30,12 @@ t.test('html-webpack-plugin', async t => {
         ],
     });
 
-    const indexFile = await fs.readFile(path.resolve(__dirname, './fixtures/output/html-webpack-plugin/index.html'), { encoding: 'utf-8' });
+    const indexFile = await fs.readFile(path.resolve(__dirname, './fixtures/output/no-html-webpack-plugin/index.html'), { encoding: 'utf-8' });
 
     t.ok(indexFile.includes('src="/app.js"'));
     t.ok(!indexFile.includes('src="https://unpkg.com/react@15.6.1/dist/react.js"'));
 
-    const output = await fs.readFile(path.resolve(__dirname, './fixtures/output/html-webpack-plugin/app.js'));
+    const output = await fs.readFile(path.resolve(__dirname, './fixtures/output/no-html-webpack-plugin/app.js'));
 
     // NOTE: not inside t.notOk to prevent ava to display whole file in console
     const doesIncludeReact = output.includes('PureComponent');
